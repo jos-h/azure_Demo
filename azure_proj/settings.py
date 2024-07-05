b64c3ae8a7fd332bfcbafe6c8219be14d44b5b33
jos-h <kunal123jamdade@gmail.com>
2024-07-05 12:43:33 +0530

    Created requirements.txt & updated the settings file

--- azure_proj/settings.py
+++ azure_proj/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-#4s+u&g^=9wauju0*nze13t9_uegma*1_bj78v^rnkun=$vxf)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
